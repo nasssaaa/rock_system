15550c684537b60718169919589a33342f43e045
wwh <1776569851@qq.com>
2026-03-29 16:08:04 +0800

    update

--- rock_stability_system/frontend/vite.config.ts
+++ rock_stability_system/frontend/vite.config.ts
@@ -9,12 +9,13 @@ export default defineConfig({
         port: 5200,
         proxy: {
             '/api': {
-                target: 'http://localhost:8000',
+                target: 'http://127.0.0.1:8000',
                 changeOrigin: true
             },
             '/ws': {
-                target: 'ws://localhost:8000',
-                ws: true
+                target: 'http://127.0.0.1:8000',
+                ws: true,
+                changeOrigin: true
             }
         }
     }
