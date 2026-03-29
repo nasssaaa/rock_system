558c97d310c9e388f8e6b303cbab87c21a355f5a
wwh <1776569851@qq.com>
2026-03-29 14:52:18 +0800

    update

--- rock_stability_system/frontend/vite.config.ts
+++ rock_stability_system/frontend/vite.config.ts
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
+    server: {
+        allowedHosts: ['wwhnb.wh1234567.com', 'localhost'],
+        host: '0.0.0.0',
+        port: 5200,
+    }
 });
